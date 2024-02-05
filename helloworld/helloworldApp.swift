2683e2a91296b1154087ce1802cb9c03baa17942
raioramalho <ramalho.sit@gmail.com>
2024-02-05 18:20:05 -0300

    Initial Commit

---
+++ helloworld/helloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloworldApp.swift
+//  helloworld
+//
+//  Created by Alan Ramalho on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct helloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
